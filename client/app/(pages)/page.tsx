a56b7350e1e3848d02cd162f6d45b7acf2fbe44c
lautaro2021 <lautaroarnay@gmail.com>
2023-11-06 12:17:13 -0300

    fix scrolleable main content

--- client/app/(pages)/page.tsx
+++ client/app/(pages)/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
 import Dashboard from "../layouts/home/Dashboard";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <>
       <Dashboard />
-    </main>
+    </>
   );
 }
