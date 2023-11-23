67527cf9912ece93d06b8485a73c21467f4d4257
lautaro2021 <lautaroarnay@gmail.com>
2023-11-23 17:20:48 -0300

    edit navbar and footer view when tu user doesnt exist

--- client/app/components/footer/Footer.tsx
+++ client/app/components/footer/Footer.tsx
@@ -8,84 +8,90 @@ import type { Claims } from "@auth0/nextjs-auth0/edge";
 function Footer({ user }: { user?: Claims }) {
   const pathname = usePathname();
 
-  if (pathname !== "/login" && pathname !== "/register") {
-    return (
-      <footer className={style.footer}>
-        <nav>
-          <img
-            src={user?.picture}
-            width={50}
-            height={50}
-            className={style.profile_picture}
-            alt="profile image icon"
-            title="profile image icon"
-          />
-          <Link href={"/pet-profile"}>
-            <button
-              style={{
-                background: `${
-                  pathname.split("/")[1] === "pet-profile"
-                    ? "rgba(77, 77, 77, 0.53)"
-                    : "none"
-                }`,
-              }}
-            >
-              <img
-                src="/assets/icons/dog.svg"
-                alt="pet icon"
-                title="pet icon"
-              />
-            </button>
-          </Link>
-          <Link href={"/"}>
-            <button
-              style={{
-                background: `${
-                  pathname === "/" ? "rgba(77, 77, 77, 0.53)" : "none"
-                }`,
-              }}
-            >
-              <img
-                src="/assets/icons/menu.svg"
-                alt="dashboard icon"
-                title="dashboard icon"
-              />
-            </button>
-          </Link>
-          <Link href={"/profile"}>
-            <button
-              style={{
-                background: `${
-                  pathname === "/profile" ? "rgba(77, 77, 77, 0.53)" : "none"
-                }`,
-              }}
-            >
-              <img
-                src="/assets/icons/profile.svg"
-                alt="profile icon"
-                title="profile icon"
-              />
-            </button>
-          </Link>
-          <div className={style.logout_container}>
-            <a href="https://polytusk.com.ar/" target="_blank" rel="noreferrer">
-              <img
-                src="/assets/icons/shop.svg"
-                alt="shop icon"
-                title="shop icon"
-              />
-            </a>
-            <a href="/api/auth/logout">
-              <img
-                src="/assets/icons/logout.svg"
-                alt="logout icon"
-                title="logout icon"
-              />
-            </a>
-          </div>
-        </nav>
-      </footer>
-    );
+  if (user) {
+    if (pathname !== "/login" && pathname !== "/register") {
+      return (
+        <footer className={style.footer}>
+          <nav>
+            <img
+              src={user?.picture}
+              width={50}
+              height={50}
+              className={style.profile_picture}
+              alt="profile image icon"
+              title="profile image icon"
+            />
+            <Link href={"/pet-profile"}>
+              <button
+                style={{
+                  background: `${
+                    pathname.split("/")[1] === "pet-profile"
+                      ? "rgba(77, 77, 77, 0.53)"
+                      : "none"
+                  }`,
+                }}
+              >
+                <img
+                  src="/assets/icons/dog.svg"
+                  alt="pet icon"
+                  title="pet icon"
+                />
+              </button>
+            </Link>
+            <Link href={"/"}>
+              <button
+                style={{
+                  background: `${
+                    pathname === "/" ? "rgba(77, 77, 77, 0.53)" : "none"
+                  }`,
+                }}
+              >
+                <img
+                  src="/assets/icons/menu.svg"
+                  alt="dashboard icon"
+                  title="dashboard icon"
+                />
+              </button>
+            </Link>
+            <Link href={"/profile"}>
+              <button
+                style={{
+                  background: `${
+                    pathname === "/profile" ? "rgba(77, 77, 77, 0.53)" : "none"
+                  }`,
+                }}
+              >
+                <img
+                  src="/assets/icons/profile.svg"
+                  alt="profile icon"
+                  title="profile icon"
+                />
+              </button>
+            </Link>
+            <div className={style.logout_container}>
+              <a
+                href="https://polytusk.com.ar/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/assets/icons/shop.svg"
+                  alt="shop icon"
+                  title="shop icon"
+                />
+              </a>
+              <a href="/api/auth/logout">
+                <img
+                  src="/assets/icons/logout.svg"
+                  alt="logout icon"
+                  title="logout icon"
+                />
+              </a>
+            </div>
+          </nav>
+        </footer>
+      );
+    }
   }
 }
 
--- client/app/components/profile-image/ProfileImage.tsx
+++ client/app/components/profile-image/ProfileImage.tsx
@@ -15,8 +15,15 @@ function ProfileImage({
   return (
     <>
       <figure className={style.figure}>
-        <input type="file" accept="image/*" onChange={handler}></input>
-        <img src={src} className={style.primary_image} />
+        <input
+          type="file"
+          accept=".png, .jpg, .jpeg"
+          onChange={handler}
+        ></input>
+        <img
+          src={src ? src : "/assets/images/default_image.png"}
+          className={style.primary_image}
+        />
         <div className={style.icon_container}>
           <MdEdit />
         </div>
