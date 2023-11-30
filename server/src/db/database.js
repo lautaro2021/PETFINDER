32819f60fa0778c3dc8c3844950b495bc1640f51
Matias Contreras <matiasjesuscontreras12@hotmail.com>
2023-11-30 13:48:35 -0300

    DB env updated

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -6,7 +6,7 @@ export const sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
     process.env.DB_PASSWORD, 
-    {host: process.env.DB_HOST || 'localhost',port: process.env.PORT, dialect: 'postgres', dialectOptions: {
+    {host: process.env.DB_HOST || 'localhost',port: process.env.DB_PORT, dialect: 'postgres', dialectOptions: {
         ssl: {
           require: true,
           rejectUnauthorized: false
