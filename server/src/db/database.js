0daddb5786fb97eff1c5e362d2bbdaa03a4c7deb
Matias Contreras <matiasjesuscontreras12@hotmail.com>
2023-11-30 13:08:32 -0300

    SSL true

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -6,5 +6,10 @@ export const sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
     process.env.DB_PASSWORD, 
-    {host: process.env.DB_HOST || 'localhost',port: process.env.PORT, dialect: 'postgres', ssl: true}
+    {host: process.env.DB_HOST || 'localhost',port: process.env.PORT, dialect: 'postgres', dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }}
 )
