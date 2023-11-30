2f244a26540313ae180394863ce96e2821eeb303
Matias Contreras <matiasjesuscontreras12@hotmail.com>
2023-11-29 21:55:56 -0300

    DB connection updated

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -6,5 +6,5 @@ export const sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
     process.env.DB_PASSWORD, 
-    {host: 'localhost', dialect: 'postgres'}
+    {host: process.env.DB_HOST || 'localhost', dialect: 'postgres'}
 )
