d46ea948d6a7914642f286045298942567134101
Matias Contreras <matiasjesuscontreras12@hotmail.com>
2023-11-30 21:28:54 -0300

    BASE URLs updated

--- client/app/utils/getData.ts
+++ client/app/utils/getData.ts
@@ -1,13 +1,14 @@
 import { getSession } from "@auth0/nextjs-auth0";
 import axios, { AxiosResponse } from "axios";
 import type { ProfileType } from "../types/profile.type";
+import { BACK_API } from "../(pages)/qr-generator/page";
 
 export const getData = async (): Promise<ProfileType> => {
   const Session = await getSession();
 
   if (Session) {
     const response = await axios.get(
-      `http://localhost:3001/petowner/login?email=${Session.user.email}&picture=${Session.user.picture}&name=${Session.user.given_name}&surname=${Session.user.family_name}`
+      `${BACK_API}/petowner/login?email=${Session.user.email}&picture=${Session.user.picture}&name=${Session.user.given_name}&surname=${Session.user.family_name}`
     );
 
     return response.data;
--- client/app/utils/getPetData.ts
+++ client/app/utils/getPetData.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import type { PetType } from "../types/pet.type";
+import { BACK_API } from "../(pages)/qr-generator/page";
 
 export const getPetData = async (id: string): Promise<PetType> => {
   const petResponse = await axios
-    .get(`http://localhost:3001/pet/${id}`)
+    .get(`${BACK_API}/pet/${id}`)
     .then((res) => res.data)
     .catch(()=>{
       return null
