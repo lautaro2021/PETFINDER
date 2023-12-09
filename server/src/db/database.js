9a644729412111d6d86553f82d5a86de61fd2649
lautaro2021 <lautaroarnay@gmail.com>
2023-12-09 14:08:03 -0300

    db change

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -5,6 +5,6 @@ dotenv.config();
 export const sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
+    String(process.env.DB_PASSWORD), 
     {host: process.env.DB_HOST || 'localhost',port: process.env.DB_PORT, dialect: 'postgres'}
 )
