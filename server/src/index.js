1d4a8e1b19bf4b53a64e039551dcdd9136cd4a85
lautaro2021 <lautaroarnay@gmail.com>
2023-12-12 17:32:03 -0300

    fix polytusk

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
+import fs from 'fs'
+import https from 'https'
 import app from "./config/app.js";
 import { sequelize } from './db/database.js';
 
@@ -6,7 +8,17 @@ dotenv.config();
 const main = async() => {
     try {
         await sequelize.sync({force:false})
-        app.listen(process.env.PORT, () => {
+
+        const privateKey = fs.readFileSync('src/ssl/key.key', 'utf-8')
+        const certificate = fs.readFileSync('src/ssl/certificate.crt', 'utf-8')
+
+        const credentials = {
+            key: privateKey,
+            cert: certificate
+        }
+
+        const server = https.createServer(credentials, app);
+        server.listen(process.env.PORT, () => {
             console.log(`listening at ${process.env.PORT}...`)
         })
     }
