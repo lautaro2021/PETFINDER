4569b57634dfa79a5cc7a109070c1f770630db1a
lautaro2021 <lautaroarnay@gmail.com>
2023-12-19 18:38:11 -0300

    update db config

--- server/src/db/database.js
+++ server/src/db/database.js
@@ -1,7 +1,15 @@
 import dotenv from 'dotenv'
 import { Sequelize } from "sequelize";
 
-dotenv.config();
+dotenv.config()
+if(process.env.NODE_ENV === 'production'){
+    console.log('##### running production database #####')
+    dotenv.config({path: '.env.prod'})
+}else{
+    console.log('##### running local database #####')
+    dotenv.config({path: '.env.dev'})
+}
+
 export const sequelize = new Sequelize(
     process.env.DB_NAME, 
     process.env.DB_USER, 
